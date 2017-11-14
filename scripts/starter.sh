b347eb4bfca37d84dbcdd8c4bf353219d876a9b7
Elek, Márton <elek@users.noreply.github.com>
2017-11-14 15:48:29 +0100

    support scm directory creation

--- scripts/starter.sh
+++ scripts/starter.sh
@@ -6,7 +6,13 @@ $DIR/envtoconf.py --destination /opt/hadoop/etc/hadoop
 if [ -n "$ENSURE_NAMENODE_DIR" ]; then
    if [ ! -d "$ENSURE_NAMENODE_DIR" ]; then
       /opt/hadoop/bin/hdfs namenode -format
-        fi
+   fi
+fi
+
+if [ -n "$ENSURE_SCM_INITIALIZED" ]; then
+   if [ ! -f "$ENSURE_SCM_INITIALIZED" ]; then
+      /opt/hadoop/bin/hdfs scm -init
+   fi
 fi
 
 $@
