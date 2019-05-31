56f3da9ddac2061ac71884c43fdc7b3ac5f67d23
Márton Elek <elek@apache.org>
2019-05-31 19:37:55 +0200

    install before acceptance tests

--- hadoop-ozone/dev-support/checks/integration.sh
+++ hadoop-ozone/dev-support/checks/integration.sh
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 export MAVEN_OPTS="-Xmx4096m"
+mvn install -f pom.ozone.xml -DskipTests
 mvn -fn test -f pom.ozone.xml -pl :hadoop-ozone-integration-test
 module_failed_tests=$(find "." -name 'TEST*.xml'\
     | xargs "grep" -l -E "<failure|<error"\
